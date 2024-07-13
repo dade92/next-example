b5daf05b433d0b9ffe3b573d0397aae4795025b5
Davide Botti <davidebotti92@gmail.com>
2024-07-13 19:34:53 +0200

    Adds loading feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,17 @@
 import '../styles/globals.css'
 import NextNProgress from 'nextjs-progressbar';
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import styled from "styled-components";
 
 const GeneralWrapper = styled.div`
-  height: 200px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <NextNProgress />
-    <GeneralWrapper><Component {...pageProps} /></GeneralWrapper>
-    </>
+export default function App({Component, pageProps}: AppProps) {
+    return <GeneralWrapper>
+        <NextNProgress/><Component {...pageProps} />
+    </GeneralWrapper>
 }
