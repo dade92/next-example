9f41d3868790cf8f471d49e7a45fbada41503dc3
Davide Botti <davidebotti92@gmail.com>
2025-01-19 09:48:18 +0100

    Some style of the detail movie

--- components/MovieDetailCard.tsx
+++ components/MovieDetailCard.tsx
@@ -2,25 +2,39 @@ import React, {FC} from "react";
 import {Comment, Movie} from "../utils/movies/Movie";
 import {CommentCard} from "./CommentCard";
 import styled from "styled-components";
-import {Typography} from "@mui/material";
+import {Card, CardContent, CardMedia, Typography} from "@mui/material";
 
 interface Props {
     movie: Movie;
     comments: Comment[];
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled(Card)`
     display: flex;
     flex-direction: column;
     gap: 16px;
+    padding: 8px;
 `
 
 export const MovieDetailCard: FC<Props> = ({movie, comments}) => {
-    return <Wrapper>
-        <Typography>{movie.title}</Typography>
-        <Typography>{movie.year}</Typography>
-        <Typography>{movie.plot}</Typography>
-        <Typography>{movie.rating}</Typography>
+    return <Wrapper sx={{ maxWidth: 700 }}>
+        <CardMedia
+            component="img"
+            height="600"
+            image={movie.posterUrl}
+            alt=""
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {movie.title}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {movie.plot}
+            </Typography>
+            <Typography variant="subtitle2" sx={{ color: 'text.secondary' }}>Year: {movie.year}</Typography>
+            <Typography variant="subtitle2" sx={{ color: 'text.secondary' }}>Rating: {movie.year}</Typography>
+        </CardContent>
+        {/*TODO put in the expand section*/}
         {comments.length == 0 && <Typography>No comments yet</Typography>}
         {comments.map((comment: Comment) => {
             return <CommentCard comment={comment}/>
