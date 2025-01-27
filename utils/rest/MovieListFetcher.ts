468e54093b886c9746cef5951d0073977a9114e5
dbotti <davide.botti@lastminute.com>
2025-01-27 08:38:25 +0100

    Better error handling

--- utils/rest/MovieListFetcher.ts
+++ utils/rest/MovieListFetcher.ts
@@ -3,6 +3,8 @@ import {SearchMovieResponse} from "../../pages/api/search";
 export const moviesFetcher = async (url: string): Promise<SearchMovieResponse> =>
     fetch(url)
         .then((res) => {
-            if (!res.ok) throw new Error(`Error: ${res.statusText}`);
+            if (!res.ok) throw {
+                status: res.status,
+            };
             return res.json();
         });
