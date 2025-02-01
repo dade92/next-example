80fbc6767baaf1f42faa450eb8c0bdd23281f20f
Davide Botti <davidebotti92@gmail.com>
2025-02-01 09:05:10 +0100

    Adds test for another use case

---
+++ src/tests/MovieDetailUseCase.test.ts
@@ -0,0 +1,22 @@
+import {moviesRepository} from "../repository/MoviesRepository";
+import {getMovieDetailsUseCase} from "../MovieDetailUseCase";
+import {MovieDetail} from "../../data/movies/Movie";
+
+
+jest.mock('../repository/moviesRepository');
+
+describe('getMovieDetailsUseCase', () => {
+    it('should return movie details for the given id', async () => {
+        const movieId = '123';
+        const response: MovieDetail = {
+            comments: []
+        };
+
+        (moviesRepository.findMovieDetail as jest.Mock).mockResolvedValue(response);
+
+        const result = await getMovieDetailsUseCase(movieId);
+
+        expect(moviesRepository.findMovieDetail).toHaveBeenCalledWith(movieId);
+        expect(result).toEqual(response);
+    });
+});
