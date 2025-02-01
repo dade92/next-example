769d960e5ae22ecd6687a9b21b665404d76972c8
Davide Botti <davidebotti92@gmail.com>
2025-02-01 11:50:21 +0100

    Adds another test

---
+++ src/tests/repository/adapters/MovieCommentAdapter.test.ts
@@ -0,0 +1,20 @@
+import {toDomainComment} from "../../../main/repository/adapters/MovieCommentAdapter";
+import {ObjectId} from "mongodb";
+
+describe('movieCommentAdapter', () => {
+    it('should adapt correctly', async () => {
+        const comment = toDomainComment({
+                _id: ObjectId.createFromHexString('573a1390f29313caabcd5a93'),
+                text: 'text',
+                email: 'email',
+                name: 'name',
+            }
+        );
+
+        expect(comment).toEqual({
+            text: 'text',
+            email: 'email',
+            name: 'name',
+        });
+    });
+});
