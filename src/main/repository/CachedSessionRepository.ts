8d6fadc2cba493332f393ea442e85ffec71d3788
Davide Botti <davidebotti92@gmail.com>
2025-02-15 15:04:46 +0100

    Fix the Cached session repo

--- src/main/repository/CachedSessionRepository.ts
+++ src/main/repository/CachedSessionRepository.ts
@@ -1,32 +1,39 @@
 import {SessionRepository} from "./SessionRepository";
 import {Session} from "../../../data/session/Session";
-import QuickLRU from 'quick-lru';
+import NodeCache from "node-cache";
+
+const TTL = 600;
 
 export class CachedSessionRepository {
 
     private delegate: SessionRepository;
-    private cache: QuickLRU<string, Session>;
+    private cache: NodeCache;
 
     constructor(delegate: SessionRepository) {
         this.delegate = delegate;
-        this.cache = new QuickLRU({maxSize: 1000});
+        this.cache = new NodeCache({stdTTL: 600, checkperiod: 120});
+    }
+
+    async addSession(
+        session: Session
+    ): Promise<any> {
+        return this.delegate.addSession(session);
     }
 
     async findSession(token: string): Promise<Session | null> {
-        const cachedSession = this.cache.get(token);
+        const cachedSession = this.cache.get(token) as Session;
 
         if (cachedSession) {
             return cachedSession
         } else {
             const session = await this.delegate.findSession(token);
             if (session) {
-                this.cache.set(token, session);
+                this.cache.set(token, session, TTL);
                 return Promise.resolve(session)
             } else {
                 return Promise.reject();
             }
         }
     }
-
 
 }
