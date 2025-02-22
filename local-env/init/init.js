f0d0f5e2c1c8c3b60f30a59c4d1d4a9989b03b41
Davide Botti <davidebotti92@gmail.com>
2025-02-22 17:12:31 +0100

    Fix init data

--- local-env/init/init.js
+++ local-env/init/init.js
@@ -3,10 +3,11 @@ db = new Mongo().getDB("sample_mflix");
 db.createCollection('movies', {capped: false});
 db.createCollection('users', {capped: false});
 db.createCollection('sessions', {capped: false});
+db.createCollection('comments', {capped: false});
 
 db.movies.insert([
     {
-        "_id": "123",
+        "_id": "573a1390f29313caabcd63d6",
         "plot": "Two peasant children, Mytyl and Tyltyl, are led by Berylune, a fairy, to search for the Blue Bird of Happiness. Berylune gives Tyltyl a cap with a diamond setting, and when Tyltyl turns the...",
         "genres": [
             "Fantasy"
@@ -58,7 +59,7 @@ db.movies.insert([
         }
     },
     {
-        "_id": "456",
+        "_id": "573a1391f29313caabcd7616",
         "plot": "Salome, the daughter of Herodias, seduces her step-father/uncle Herod, governor of Judea, with a salacious dance. In return, he promises her the head of the prophet John the Baptist.",
         "genres": [
             "Biography",
@@ -117,96 +118,41 @@ db.movies.insert([
         }
     },
     {
-        "_id": "789",
-        "plot": "Christ takes on the form of a pacifist count to end a senseless war.",
+        "_id": "573a1390f29313caabcd4eaf",
+        "plot": "A woman, with the aid of her police officer sweetheart, endeavors to uncover the prostitution ring that has kidnapped her sister, and the philanthropist who secretly runs it.",
         "genres": [
+            "Crime",
             "Drama"
         ],
-        "runtime": 78,
+        "runtime": 88,
         "cast": [
-            "Howard C. Hickman",
-            "Enid Markey",
-            "Lola May",
-            "Kate Bruce"
+            "Jane Gail",
+            "Ethel Grandin",
+            "William H. Turner",
+            "Matt Moore"
         ],
-        "num_mflix_comments": 0,
-        "poster": "https://m.media-amazon.com/images/M/MV5BMjAwNTIxMjE5N15BMl5BanBnXkFtZTgwODc1Mjg1MzE@._V1_SY1000_SX677_AL_.jpg",
-        "title": "Civilization",
-        "fullplot": "Allegorical film about peace. A king starts a war, many of the women are against it, people are pressed into service. A count has constructed a submarine and gets the order to sink an ocean liner, that is also carrying - supposedly - ammunition for the enemy. The count refuses to fire the torpedos, and sinks the submarine. He survives, but in a limbo between death and life where he meets Jesus, who takes him over to preach peace. Naturally the king arrests him and sentences him to death for treason, but then Jesus shows him the real face of war.",
-        "countries": [
-            "USA"
-        ],
-        "released": new Date(2015, 1, 3),
-        "directors": [
-            "Reginald Barker",
-            "Thomas H. Ince",
-            "Raymond B. West",
-            "Walter Edwards",
-            "David Hartford",
-            "Jay Hunt",
-            "J. Parker Read Jr."
-        ],
-        "writers": [
-            "C. Gardner Sullivan"
-        ],
-        "awards": {
-            "wins": 1,
-            "nominations": 0,
-            "text": "1 win."
-        },
-        "lastupdated": "2015-04-30 02:13:27.863000000",
-        "year": 1916,
-        "imdb": {
-            "rating": 6.3,
-            "votes": 162,
-            "id": 6517
-        },
-        "type": "movie",
-        "tomatoes": {
-            "viewer": {
-                "rating": 0,
-                "numReviews": 7
-            },
-        }
-    },
-    {
-        "_id": "012",
-        "plot": "An immigrant leaves his sweetheart in Italy to find a better life across the sea in the grimy slums of New York. They are eventually reunited and marry. But life in New York is hard and ...",
-        "genres": [
-            "Drama"
-        ],
-        "runtime": 78,
-        "rated": "PASSED",
-        "cast": [
-            "George Beban",
-            "Clara Williams",
-            "J. Frank Burke",
-            "Leo Willis"
-        ],
-        "title": "The Italian",
-        "fullplot": "An immigrant leaves his sweetheart in Italy to find a better life across the sea in the grimy slums of New York. They are eventually reunited and marry. But life in New York is hard and tragedy tarnishes their dream of a better life in the new world.",
+        "num_mflix_comments": 1,
+        "poster": "https://m.media-amazon.com/images/M/MV5BYzk0YWQzMGYtYTM5MC00NjM2LWE5YzYtMjgyNDVhZDg1N2YzXkEyXkFqcGdeQXVyMzE0MjY5ODA@._V1_SY1000_SX677_AL_.jpg",
+        "title": "Traffic in Souls",
+        "lastupdated": "2015-09-15 02:07:14.247000000",
         "languages": [
             "English"
         ],
-        "released": new Date(2015, 2, 2),
+        "released": new Date(2015, 3, 1),
         "directors": [
-            "Reginald Barker"
+            "George Loane Tucker"
         ],
-        "writers": [
-            "Thomas H. Ince (story)",
-            "C. Gardner Sullivan (story)"
-        ],
+        "rated": "TV-PG",
         "awards": {
             "wins": 1,
             "nominations": 0,
             "text": "1 win."
         },
-        "lastupdated": "2015-07-27 00:07:43.230000000",
-        "year": 1915,
+        "year": 1913,
         "imdb": {
-            "rating": 6.4,
-            "votes": 175,
-            "id": 5557
+            "rating": 6,
+            "votes": 371,
+            "id": 3471
         },
         "countries": [
             "USA"
@@ -214,15 +160,14 @@ db.movies.insert([
         "type": "movie",
         "tomatoes": {
             "viewer": {
-                "rating": 4,
-                "numReviews": 204,
-                "meter": 60
+                "rating": 3,
+                "numReviews": 85,
+                "meter": 57
             },
             "dvd": {
                 "$date": "2008-08-26T00:00:00.000Z"
             },
-        },
-        "num_mflix_comments": 0
+        }
     }
 ]);
 
