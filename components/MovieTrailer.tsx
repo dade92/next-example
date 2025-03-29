980d29fe0b32b36492458bd65fb213aad4b6608d
Davide Botti <davidebotti92@gmail.com>
2025-03-29 10:51:35 +0100

    Refactor

--- components/MovieTrailer.tsx
+++ components/MovieTrailer.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import {FC} from "react";
+import styled from "styled-components";
 
 interface Props {
     trailerUrl: string;
 }
 
-export const MovieTrailer: FC<Props> = ({trailerUrl}) => {
-    return <>
+const Wrapper = styled.div`
+    margin-top: 24px;
+`
+
+export const MovieTrailer: FC<Props> = ({trailerUrl}) =>
+    <Wrapper>
         <iframe
             width="560"
             height="315"
@@ -15,5 +20,4 @@ export const MovieTrailer: FC<Props> = ({trailerUrl}) => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
         ></iframe>
-    </>
-}
+    </Wrapper>
