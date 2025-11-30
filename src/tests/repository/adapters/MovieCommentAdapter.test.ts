06f5664b45371f6a9d66dceee3d7c39cefb82547
Davide Botti <davidebotti92@gmail.com>
2025-11-30 09:58:01 +0100

    Fix test

--- src/tests/repository/adapters/MovieCommentAdapter.test.ts
+++ src/tests/repository/adapters/MovieCommentAdapter.test.ts
@@ -1,19 +1,19 @@
 import {toDomainComment} from "../../../main/repository/adapters/MovieCommentAdapter";
-import {ObjectId} from "mongodb";
 
 describe('movieCommentAdapter', () => {
     const text = 'text';
     const email = 'email';
     const name = 'name';
     
-    it('should adapt correctly', async () => {
-        const comment = toDomainComment({
-                _id: ObjectId.createFromHexString('573a1390f29313caabcd5a93'),
-                text: text,
-                email: email,
-                name: name,
-            }
-        );
+    it('should adapt correctly', () => {
+        const mockMongoComment = {
+            _id: 'mock-object-id',
+            text: text,
+            email: email,
+            name: name,
+        };
+
+        const comment = toDomainComment(mockMongoComment as any);
 
         expect(comment).toEqual({
             text: text,
